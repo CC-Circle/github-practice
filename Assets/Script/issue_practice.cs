1bcaa4f4e7c069ea6494558ac74b1eafeea440bd
Maito Yasui <k22137kk@aitech.ac.jp>
2024-02-09 15:54:58 +0900

    feat: Consoleにint型の配列の要素を表示

--- Assets/Script/issue_practice.cs
+++ Assets/Script/issue_practice.cs
@@ -7,12 +7,17 @@ public class issue_practice : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int[] a = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < 6; i++)
+        {
+            Debug.Log(a[i]);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
     }
 }
