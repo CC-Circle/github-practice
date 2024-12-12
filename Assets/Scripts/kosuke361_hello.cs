751f7aa62fb7849598e37a46ebb1cf52d8a144d1
kosuke361 <mars.1.881@icloud.com>
2024-12-12 16:09:51 +0900

    kosuke361_hello

---
+++ Assets/Scripts/kosuke361_hello.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kosuke361_hello : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+}
