aad4a1c6548f107c7447c6cee55fddfb14213867
chisato <x24014xx@aitech.ac.jp>
2024-12-12 16:39:48 +0900

    C#スクリプトファイルの作成

---
+++ Assets/Scripts/chisato.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class chisato : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello, Chisato!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
